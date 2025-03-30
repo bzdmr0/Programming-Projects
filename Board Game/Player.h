17a119211cbc0254055f53ec4af23e7bdc8878ee
Sait Bozdemir <bzdmrsaid@gmail.com>
2025-03-30 21:28:42 +0300

    Player.h

--- Board Game/Player.h
+++ Board Game/Player.h
@@ -1,12 +1,10 @@
-/** Arda Menşur – 2585214 , Muhammet Sait Bozdemir – 2584738
-We read and accept the submission rules and the extra rules specified in each question. This is our own work that is done by us only **/
+
 #ifndef INC_242ASSIGNMENT3_PLAYER_H
 #define INC_242ASSIGNMENT3_PLAYER_H
 
 using namespace std;
 #include <iostream>
 #include "Elements.h"
-/** We created this class together **/
 struct node { //this structure holds the player stats
     int goldCount;
     int woodCount;
