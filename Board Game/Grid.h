e34317480611219d205f5c1ea9a0458c9825147b
Sait Bozdemir <bzdmrsaid@gmail.com>
2025-03-30 21:28:08 +0300

    Grid.h

--- Board Game/Grid.h
+++ Board Game/Grid.h
@@ -1,5 +1,4 @@
-/** Arda Menşur – 2585214 , Muhammet Sait Bozdemir – 2584738
-We read and accept the submission rules and the extra rules specified in each question. This is our own work that is done by us only **/
+
 #ifndef INC_242ASSIGNMENT3_GRID_H
 #define INC_242ASSIGNMENT3_GRID_H
 #include "Elements.h"
@@ -28,19 +27,12 @@ public:
     int getHealthP2();
     int getScoreP1();
     int getScoreP2();
-    //created by Arda
     void deploy_elements();
-    //created by Sait
     void movement(int V, int H,int turn);
-    // created by Arda
     bool isavailable(int V,int H);
-    //Created by Sait
     bool isEnd(int totalTurn,int turn);
-    //Created by Arda
     void behind(int V,int H);
-    //Created by Sait
     void printBoard(int turn,int totalturn);
-    //We created together.
     bool isEmpty(int posH, int posV);
 
 };
