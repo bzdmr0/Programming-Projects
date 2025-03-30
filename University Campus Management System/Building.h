0e4acde227abd318535a7fb497a9feeb8d1cee8e
Sait Bozdemir <bzdmrsaid@gmail.com>
2025-03-30 21:57:33 +0300

    Update Building.h

--- University Campus Management System/Building.h
+++ University Campus Management System/Building.h
@@ -1,6 +1,3 @@
-/** Muhammet Sait Bozdemir -- 2584738
-I read and accept the submission rules and the extra rules specified in each
-question. This is my own work that is done by myself only**/
 
 #ifndef ASSGN2_BUILDING_H
 #define ASSGN2_BUILDING_H
