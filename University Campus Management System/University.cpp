3b9f2b8f405db857441f2f070a1758503c480a2f
Sait Bozdemir <bzdmrsaid@gmail.com>
2025-03-30 21:58:51 +0300

    Update University.cpp

--- University Campus Management System/University.cpp
+++ University Campus Management System/University.cpp
@@ -1,6 +1,3 @@
-/** Muhammet Sait Bozdemir -- 2584738
-I read and accept the submission rules and the extra rules specified in each
-question. This is my own work that is done by myself only**/
 
 #include "University.h"
 
